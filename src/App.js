31c6e240e4d0118ebf3628f973462d9b553fe359
FrozenTear7 <RainbowPablo7@gmail.com>
2018-04-22 13:12:58 +0200

    POST not working properly cors errors GET gives a result list rendered in a list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react'
 import axios from 'axios'
 
+const responseFromPost = {
+  'result': ['9a69d65b6e5aa19dbfb356d0a8704bb9adc74ce841267fb13bba6532cb094c9f', '323a03939464957a8509049c539ae4fe1c679d31596cdfe13ce6434061407336'],
+  'id': 'waokcy',
+  'total': 2,
+}
+
 class App extends Component {
   constructor() {
     super()
@@ -8,8 +14,44 @@ class App extends Component {
     this.state = {
       itemDesc: '',
       itemName: '',
+      itemType: '',
       itemMods: [],
-      data: '',
+      league: 'Bestiary',
+      resultList: [],
+    }
+  }
+
+  mapResultList = (list) => {
+    if (list.length <= 0)
+      return (
+        <h4>No results</h4>
+      )
+    else {
+      console.log(list)
+      return (
+        <div>
+          <h3>Best results:</h3>
+          <ul className='list-group'>
+            {list.map(item => {
+              return (
+                <div className='dropdown'>
+                  <li key={item.id}
+                      className='list-group-item dropdown-toggle' type="button"
+                      id={item.id} data-toggle="dropdown" aria-haspopup="true"
+                      aria-expanded="false">{item.info.price.amount} - {item.info.price.currency}</li>
+                  <div className="dropdown-menu" aria-labelledby={item.id}>
+                    {item.item.explicitMods.map(mod => {
+                      return (
+                        <li className="dropdown-item" key={mod}>{mod}</li>
+                      )
+                    })}
+                  </div>
+                </div>
+              )
+            })}
+          </ul>
+        </div>
+      )
     }
   }
 
@@ -25,9 +67,10 @@ class App extends Component {
 
     if (state.itemDesc !== '') {
       //read item name
-      if (/Rarity: .*\n.*\n*/.test(state.itemDesc))
+      if (/Rarity: .*\n.*\n*/.test(state.itemDesc)) {
         state.itemName = state.itemDesc.match(/Rarity: .*\n.*\n/)[0]
-      else {
+        state.itemType = state.itemDesc.match(/Rarity: .*\n.*\n.*\n/)[0]
+      } else {
         this.clearItem()
         return
       }
@@ -37,7 +80,8 @@ class App extends Component {
         return
       }
 
-      state.itemName = state.itemName.match(/\n.*/)[0].substr(1, state.itemName.match(/\n.*/)[0].length - 1)
+      state.itemName = state.itemName.split('\n')[1]
+      state.itemType = state.itemType.split('\n')[2]
 
       //read item mods
       state.itemMods = state.itemDesc.split('\n')
@@ -67,32 +111,28 @@ class App extends Component {
 
       state.itemMods = state.itemMods.slice(startIndex, endIndex)
 
-      const nameRegex = new RegExp('.*' + state.itemName + '.*')
+      /*
+            axios.post('http://www.pathofexile.com/api/trade/search/Bestiary', {
+              headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+              },
+              data: {
+                'query': {
+                  'status': {'option': 'online'},
+                  'name': state.itemName,
+                  'type': state.itemType',
+                  'stats': [{'type': 'and', 'filters': []}],
+                }, 'sort': {'price': 'asc'},
+              },
+            })*/
 
-      axios.get('http://api.pathofexile.com/public-stash-tabs')
-        .then(response => {
-            let resultArr = []
-
-            if (response.data && response.data.stashes) {
-              response.data.stashes
-                .filter(stash => {
-                  if (stash && stash.items.length > 0) {
-                    return stash
-                  }
-                })
-                .map(stash => stash.items)
-                .forEach(items => items.forEach(item => {
-                  if (item.name && nameRegex.test(item.name) && item.note)
-                    resultArr.push({name: item.name, mods: item.explicitMods, note: item.note})
-                }))
-            }
-            this.setState({data: resultArr})
-          },
-        )
+      axios.get(`https://www.pathofexile.com/api/trade/fetch/${(responseFromPost.result.join()).replace(/'/gi, '')}?query=${responseFromPost.id}`)
+        .then(response => this.setState({...this.state, resultList: response.data.result}))
 
       this.setState(state)
-
-      console.log(this.state)
     }
   }
 
@@ -100,23 +140,31 @@ class App extends Component {
     const state = this.state
     state.itemDesc = ''
     state.itemName = ''
+    state.itemType = ''
     state.itemMods = []
+    state.resultList = []
     this.setState(state)
   }
 
   render() {
-    const {itemDesc} = this.state
+    const {itemDesc, resultList} = this.state
 
     return (
-      <div className='container'>
-        <h1 className='text-center'>
-          Poe unique price
-        </h1>
+      <div className='container-fluid'>
+        <nav className='navbar navbar-expand-lg navbar-light bg-dark'>
+          <a className='navbar-brand' style={{color: 'white'}}>Poe unique price</a>
+        </nav>
         <br/><br/><br/>
         <div className='row text-center'>
           <div className='col-lg-6'>
             <form onSubmit={this.onSubmit}>
               <div className='form-group'>
+                <select className='custom-select mb-2 mr-sm-2 mb-sm-0' name='league' defaultValue='Bestiary'
+                        onChange={this.onChange}>
+                  <option value='Bestiary'>Bestiary</option>
+                  <option value='Standard'>Standard</option>
+                </select>
+                <br/>
                 <label>Paste the unique item below you want to price check</label>
                 <textarea className='form-control' style={{height: 600, fontSize: 12}} name='itemDesc'
                           placeholder='Item description' value={itemDesc} onChange={this.onChange}/>
@@ -129,7 +177,7 @@ class App extends Component {
           <div className='col-lg-6'>
             <h2>Estimated price: </h2>
             <br/>
-            Item list
+            {this.mapResultList(resultList)}
           </div>
         </div>
       </div>
